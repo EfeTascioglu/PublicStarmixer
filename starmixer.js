b52e2b3dfc45755c908e6ea7de95788b2d4606a9
omegalink12 <omegalink12@users.noreply.github.com>
2019-09-07 22:12:23 -0400

    Fix type

--- starmixer.js
+++ starmixer.js
@@ -1,8 +1,9 @@
-const GATHERING_TIMEOUT = 5 * 1000; // amount of time to wait to gather users
-const SIGNING_TIMEOUT = 10 * 1000;
 const c = require("./config.json");
 const t = require("./transaction");
 const s = require("./shuffle");
+
+const GATHERING_TIME = 5 * 1000; // amount of time to wait to gather users
+const SIGNING_TIME = 10 * 1000;
 
 
 //help line
@@ -57,7 +58,7 @@ bot
 	}
 	  // Valid form
 	   if (args.length >= 2 ){
-		let amount = parseInt(args[1]);
+		let amount = args[1].replace("xlm","");
 		let targetUsername = args[2];
 
 		// Trim '@' off of it
@@ -70,21 +71,20 @@ bot
 		console.log("                  target is:", targetUsername);
 
 		// TODO check more validations
-		if (amount >= 0 && !isNaN(amount)){
+		if (parseInt(amount) >= 0 && !isNaN(parseInt(amount))){
 
 			try {
 				// If lookup fails, error will be thrown
 				let fromWallet = await bot.wallet.lookup(message.sender.username);
 				let toWallet = await bot.wallet.lookup(targetUsername);
 
-				// Valid statment, start waiting for timeout
+				// Valid statment
 				if (state === "idle") {
 
 					console.log("Switching to gathering state!");
 					state = "gathering";
 
-					// Begin the transaction signing
-					setTimeout(transact, GATHERING_TIMEOUT); 
+					setTimeout(transact, GATHERING_TIME); 
 				}
 
 
@@ -107,7 +107,7 @@ bot
 		}
 
 	   } else{
-		bot.chat.send(channel, {body: "Enter: !mix <val> xlm <arg>"});
+		bot.chat.send(channel, {body: "Enter: !mix <val>xlm <arg>"});
 	   }
 
        } else {
@@ -150,32 +150,7 @@ async function transact() {
 
 	console.log("Transactions occurring: ", transactions);
 
-	// Add a timeout for signing
-	setTimeout(resetState, SIGNING_TIMEOUT);
+
 }
 
-
-// Resets everything to original state
-function resetState() {
-
-
-        // Send a message to all transacts that signing timed out.
-        for (transaction of transactions) {
-                let name = transaction.fromUsername + "," + bot.myInfo().username;
-                console.log("Channel name is:", name);
-                let channel = {
-                        name: name,
-                        public: false,
-                        topicType: "chat"
-                };
-                bot.chat.send(channel, {body:"Transaction signing timed out, as not everyone signed. Type '!mix' to start again!"});
-        }
-
-
-
-
-	transactions = [];
-	signatures = [];
-	state = "idle";
-}
 
