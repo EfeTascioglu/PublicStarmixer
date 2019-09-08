333b64a24f97d3ae86c7567720b52586a8937eaf
omegalink12 <omegalink12@users.noreply.github.com>
2019-09-07 22:50:13 -0400

    u

--- starmixer.js
+++ starmixer.js
@@ -3,12 +3,12 @@ const t = require("./transaction");
 const s = require("./shuffle");
 
 const GATHERING_TIME = 5 * 1000; // amount of time to wait to gather users
-const SIGNING_TIME = 10 * 1000;
-
 
 //help line
-function help(){
-	bot.chat.send(channel, {body: "Enter: !mix <val> xlm <arg>"});
+function help() {
+    bot.chat.send(channel, {
+        body: "Enter: !mix <val> xlm <arg>"
+    });
 }
 
 let state = "idle";
@@ -20,140 +20,181 @@ let transactions = [];
 // Signing signatures
 let signatures = [];
 
+let xdr = null;
+
 const Bot = require('keybase-bot')
 
 const bot = new Bot()
 const username = c.botUser;
 const paperkey = c.botKey;
 bot
-  .init(username, paperkey, {verbose: false})
-  .then(async () => {
-	await t.load()
-    console.log(`Your bot is initialized. It is logged in as ${bot.myInfo().username}`)
+    .init(username, paperkey, {
+        verbose: false
+    })
+    .then(async () => {
+        await t.load()
+        console.log(`Your bot is initialized. It is logged in as ${bot.myInfo().username}`)
 
-    const onMessage = async message => {
-       const channel = message.channel;
+        const onMessage = async message => {
+            const channel = message.channel;
 
-       let args = message.content.text.body.split(" ");
-       console.log("args: ", args);
-
-
-	// Handle signing logic
-	if (args[0].toLowerCase().localeCompare("!sign") === 0) {
-
-		if (state !== "signing") {
-			bot.chat.send(channel, {body: "Not currently signing!"});
-			return;
-		}
+            let args = message.content.text.body.split(" ");
+            console.log("args: ", args);
 
 
-		// TODO sign
-	}
+            // Handle signing logic
+            if (args[0].toLowerCase().localeCompare("!sign") === 0) {
 
-       if (args[0].toLowerCase().localeCompare("!mix") === 0) {
+                if (state !== "signing") {
+                    bot.chat.send(channel, {
+                        body: "Not currently signing!"
+                    });
+                    return;
+                }
 
-	if (state === "signing") {
-		bot.chat.send(channel, {body: "Signing in progress! Try again later!"});
-		return;
-	}
-	  // Valid form
-	   if (args.length >= 2 ){
-		let amount = args[1].replace("xlm","");
-		let targetUsername = args[2];
+                let fromWallet = await bot.wallet.lookup(message.sender.username);
 
-		// Trim '@' off of it
-		if (targetUsername.charAt(0) === '@') {
-			console.log("Removing an '@'");
-			targetUsername = targetUsername.substring(1);
-		}
+                signatures.push([fromWallet.accountId, args[1]])
+                if (signatures.length === transactions.length) {
+                    const res = await t.signTransaction(xdr, signatures)
+                    // Send a message to all signers
+                    for (transaction of transactions) {
 
-		console.log("Message received: amount is:", amount);
-		console.log("                  target is:", targetUsername);
+                        let name = transaction.fromUsername + "," + bot.myInfo().username;
+                        console.log("Channel name is:", name);
+                        let channel = {
+                            name: name,
+                            public: false,
+                            topicType: "chat"
+                        };
+                        bot.chat.send(channel, {
+                            body: "Success! Transaction ID: " + res.hash
+                        });
+                    }
 
-		// TODO check more validations
-		if (parseInt(amount) >= 0 && !isNaN(parseInt(amount))){
+                    transactions = []
+                    signatures = []
+                    state = "idle"
+                    xdr = null
+                }
+            }
 
-			try {
-				// If lookup fails, error will be thrown
-				let fromWallet = await bot.wallet.lookup(message.sender.username);
-				let toWallet = await bot.wallet.lookup(targetUsername);
+            if (args[0].toLowerCase().localeCompare("!mix") === 0) {
 
-				// Valid statment
-				if (state === "idle") {
+                if (state === "signing") {
+                    bot.chat.send(channel, {
+                        body: "Signing in progress! Try again later!"
+                    });
+                    return;
+                }
+                // Valid form
+                if (args.length >= 2) {
+                    let amount = args[1].replace("xlm", "");
+                    let targetUsername = args[2];
 
-					console.log("Switching to gathering state!");
-					state = "gathering";
+                    // Trim '@' off of it
+                    if (targetUsername.charAt(0) === '@') {
+                        console.log("Removing an '@'");
+                        targetUsername = targetUsername.substring(1);
+                    }
 
-					setTimeout(transact, GATHERING_TIME); 
-				}
+                    console.log("Message received: amount is:", amount);
+                    console.log("                  target is:", targetUsername);
+
+                    // TODO check more validations
+                    if (parseFloat(amount) >= 0 && !isNaN(parseFloat(amount))) {
+
+                        try {
+                            // If lookup fails, error will be thrown
+                            let fromWallet = await bot.wallet.lookup(message.sender.username);
+                            let toWallet = await bot.wallet.lookup(targetUsername);
+
+                            // Valid statment
+                            if (state === "idle") {
+
+                                console.log("Switching to gathering state!");
+                                state = "gathering";
+
+                                setTimeout(transact, GATHERING_TIME);
+                            }
 
 
-				transactions.push({
-					to: toWallet.accountId,
-					toUsername: targetUsername,
-					from: fromWallet.accountId,
-					fromUsername: message.sender.username,
-					value: amount
-				});
-				bot.chat.send(channel, {body: "Transaction added to queue!"});
+                            transactions.push({
+                                to: toWallet.accountId,
+                                toUsername: targetUsername,
+                                from: fromWallet.accountId,
+                                fromUsername: message.sender.username,
+                                value: amount
+                            });
+                            bot.chat.send(channel, {
+                                body: "Transaction added to queue!"
+                            });
 
-			} catch (err) {
-				bot.chat.send(channel, {body: "Sorry, we couldn't find that user!"});
-//				console.log("Error grabbing wallet",err);
-			}
+                        } catch (err) {
+                            bot.chat.send(channel, {
+                                body: "Sorry, we couldn't find that user!"
+                            });
+                            //				console.log("Error grabbing wallet",err);
+                        }
 
-		} else {
-			bot.chat.send(channel, {body: "Oh No Yo! Your number isn't valid!"});
-		}
+                    } else {
+                        bot.chat.send(channel, {
+                            body: "Oh No Yo! Your number isn't valid!"
+                        });
+                    }
 
-	   } else{
-		bot.chat.send(channel, {body: "Enter: !mix <val>xlm <arg>"});
-	   }
+                } else {
+                    bot.chat.send(channel, {
+                        body: "Enter: !mix <val>xlm <arg>"
+                    });
+                }
 
-       } else {
-           bot.chat.send(channel, {body: "Hi! Please Type: '!mix'"});
-       }
-    }
+            } else {
+                bot.chat.send(channel, {
+                    body: "Hi! Please Type: '!mix'"
+                });
+            }
+        }
 
-    bot.chat.watchAllChannelsForNewMessages(onMessage);
+        bot.chat.watchAllChannelsForNewMessages(onMessage);
 
     })
-  .catch(error => {
-    console.error(error)
-    bot.deinit()
-  })
+    .catch(error => {
+        console.error(error)
+        bot.deinit()
+    })
 
 
 // Does transactions between everyone in array
-async function transact() {
-try{
-	console.log("Transaction occurring! Switching to signing state!");
-	state = "signing";
+function transact() {
+    try {
+        console.log("Transaction occurring! Switching to signing state!");
+        state = "signing";
 
-	let transaction;
-	const xdr = t.buildTransaction(s(transactions));
+        let transaction;
+        xdr = t.buildTransaction(s(transactions));
 
 
 
-	// Send a message to all signers
-	for (transaction of transactions) {
+        // Send a message to all signers
+        for (transaction of transactions) {
 
-		let name = transaction.fromUsername + "," + bot.myInfo().username;
-		console.log("Channel name is:", name);
-		let channel = {
-			name: name,
-			public: false,
-			topicType: "chat"
-		};
-		bot.chat.send(channel, {body:"Type `keybase wallet sign --xdr "+xdr+" --account "+transaction.from+"` to sign the transaction to "
-				 + transaction.toUsername + " for " + transaction.value + " lumens! \n Then send the signature to the bot with `!sign <signature>`"});
-	}
+            let name = transaction.fromUsername + "," + bot.myInfo().username;
+            console.log("Channel name is:", name);
+            let channel = {
+                name: name,
+                public: false,
+                topicType: "chat"
+            };
+            bot.chat.send(channel, {
+                body: "Type `keybase wallet sign --xdr " + xdr + " --account " + transaction.from + "` to sign the transaction to " +
+                    transaction.toUsername + " for " + transaction.value + " lumens! \n Then send the signature to the bot with `!sign <signature>`"
+            });
+        }
 
-	console.log("Transactions occurring: ", transactions);
-}catch(e){
-console.log(e);
+        console.log("Transactions occurring: ", transactions);
+    } catch (e) {
+        console.log(e);
+    }
+
 }
-
-}
-
-
